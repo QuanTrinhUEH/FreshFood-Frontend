eea24074fd6a882c6b91eb551671c7e220f446fd
Quan <143281625+QuanTrinhUEH@users.noreply.github.com>
2024-10-24 14:04:07 +0700

    Add about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import '../css/About.scss'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about-page">
+      <h1>Giới thiệu</h1>
+      <div className="about-content">
+        <h2>TIÊU CHÍ CỦA SHOP THỰC PHẨM SẠCH LÀ CHỈ SỬ DỤNG THỰC PHẨM SẠCH, RÕ NGUỒN GỐC, KHÔNG HÓA CHẤT NHẰM BẢO VỆ SỨC KHỎE NGƯỜI TIÊU DÙNG</h2>
+        
+        <p>Thưa các mẹ,</p>
+        
+        <p>Thật ra, Shop THỰC PHẨM SẠCH ra đời là vì mẹ chậu đã có sở thích từ lâu; bởi vốn bản tính năng động và... thích làm cái gì đó để chia sẻ với bà con, bạn bè. Đang loay hoay mở lối thì vấn đề an toàn thực phẩm trở thành mối quan tâm chung và đặc biệt quan trọng với chúng ta. Bởi nếu không cố gắng để kiểm soát nó thì sức khỏe chúng ta bị đánh đổi hàng ngày qua bữa ăn, thậm chí tính mạng các thế hệ con cái rất dễ bị đe dọa. Mặt khác, về mặt kinh tế, một đồng chi cho phòng ngừa sẽ có thể tiết kiệm chỉ hàng triệu đồng cho khắc phục hậu quả.</p>
+        
+        <p>Bởi thế nhà mình hay lo xa, lại thích ăn ngon (hic), hay tự tìm nguồn thực phẩm sạch cho gia đình. Và từ đó ý tưởng mở lối "làm cái gì đó" dần dặt. Câu hỏi đặt ra là tại sao kết nối được nguồn thực phẩm sạch và ngon, trong khi bạn bè, người quen cũng cần như mình, mà lại không đáp ứng? Nếu cố gắng tìm cách đáp ứng nhu cầu này của các mẹ, được các mẹ hưởng ứng và chia sẻ, thì hai bên và cả nhà đều vui, có phải không?</p>
+        
+        <p>Vậy nên các mẹ hãy yên tâm, trước khi Thực phẩm sạch đến tay các mẹ, nhà mình đã thử nghiệm và kiểm tra thậm chí từ vài lần đến hàng năm nay rồi nhé. Ví dụ như hải sản Phú Quốc là nguồn thực phẩm sạch và tươi ngon, được đánh bắt bởi thuyền nhỏ gần bờ, sản phẩm chưa qua ướp lạnh lâu ngày như các tàu thuyền đánh bắt xa bờ, nhà mình đã kiểm tra, hướng dẫn cách chọn mua và cấp đông ngay khi tôm cá đang quẫy.</p>
+        
+        <p>Với phương châm nhà mình dùng gì thì phục vụ bạn bè, các mẹ cũng như thế. Tuy nhiên do đặc thù nguồn hàng được chọn kỹ và chủ yếu nguồn gốc tự nhiên, cho nên có thể không phải lúc nào cũng đáp ứng được như câu "có ngay, có ngay" của các mẹ. Bởi vì hải sản biển thì phụ thuộc vào con nước, tuần trăng (không phải tuần trăng mật nhé các mẹ !), ngay cả tôm cá đồng cũng không thường xuyên có, mà nên đặt trước.</p>
+        
+        <p>Kính các mẹ và rất mong các mẹ ủng hộ, mong tất cả chúng ta và cộng đồng luôn có bữa ăn với THỰC PHẨM SẠCH , ngon và sức khỏe tốt.</p>
+        
+        <p>Chúc cho các gia đình chúng ta luôn vui, khỏe với những bữa ăn ngon với THỰC PHẨM SẠCH !</p>
+      </div>
+    </div>
   )
 }
 
